e1c3603a5b79c38c8184a80f7c61ab9ec73cc8d6
tqmane <nomu78@iwink.jp>
2026-03-01 16:25:58 +0900

    Bump up version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.tqmane.filmsim"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.1.2"
+        versionCode = 16
+        versionName = "1.1.3"
 
         // Read ASSET_KEY from secrets.properties (or use a fallback for external contributors)
         val secretsFile = rootProject.file("secrets.properties")
